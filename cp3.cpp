1e2c727c186a15666268a8736a498b131eb6fa2a
sumeet <sumeethukkeri@gmail.com>
2026-03-13 15:56:51 +0530

    Add files via upload

---
+++ cp3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+ using namespace std;
+int main()
+{
+
+ int a,b;
+ cout<<" enter the value of a and b:";
+ cin>>a>>b;
+ cout<<"\nsum is\n "<<a+b;
+  cout<<" \ndiffernce is\n "<<a-b;
+  cout<<" \nmultiplication is\n "<<a*b;
+  cout<<" \ndivision is\n "<<(float)a/b;
+  cout<<"\nmodulus is\n "<<a%b;
+
+ return 0;
+}
+
+   
+
+
+
+
+
